b985995530f2404c59e8826ab820ff7c7e5479cf
wangjiangang <wangjiangang@sinoiov.com>
2019-09-29 17:18:20 +0800

    初步完成音乐播放

--- miniprogram/pages/weather/weather.js
+++ miniprogram/pages/weather/weather.js
@@ -1,4 +1,4 @@
-// miniprogram/pages/index/index.js
+// miniprogram/pages/weather/weather.js
 Page({
 
   /**
@@ -6,26 +6,20 @@ Page({
    */
   data: {
     temperature: {},
-    poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
-    name: '此时此刻',
-    author: '许巍',
-    src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.requestData()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    // 使用 wx.createAudioContext 获取 audio 上下文 context
-    this.audioCtx = wx.createAudioContext('myAudio');
-    this.audioCtx.play()
+
   },
 
   /**
@@ -34,7 +28,27 @@ Page({
   onShow: function () {
 
   },
-
+  requestData() {
+    let _this = this;
+    wx.request({
+      url: "https://free-api.heweather.net/s6/weather/now?location=beijing&key=e3d066620f0245bcb7e8425f18f333c4",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (e) {
+        let {
+          data,
+          statusCode
+        } = e;
+        if (statusCode == "200") {
+          _this.setData({
+            temperature: data.HeWeather6[0]
+          })
+        }
+        console.log(e)
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
@@ -48,10 +62,9 @@ Page({
   onUnload: function () {
 
   },
- 
+
   /**
    * 页面相关事件处理函数--监听用户下拉动作
-   * 
    */
   onPullDownRefresh: function () {
 
